062debd7379a6c6222a6e0dd04083001f6f36de9
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-09-03 15:35:50 +0800

    package modal form

--- src/routes/userMgmt/index.js
+++ src/routes/userMgmt/index.js
@@ -4,11 +4,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "dva";
-import {Modal, Row, Col, Card, Button} from "antd";
+import {Select, Row, Col, Card, Button, Input, Icon} from "antd";
+
 import DataTable from "../../components/BasicTable/DataTable";
 import DropOption from "../../components/DropOption/DropOption";
-import {ActionCollections} from "../../components/host/ActionCollections";
 import AddUserModal from "./modal/addUser";
+import {fetchAndNotification} from "../../services/restfulService";
 
 class UserMgmt extends React.Component {
 
@@ -69,7 +70,111 @@ class UserMgmt extends React.Component {
     };
 
     this.AddUserModal = {
-      refresh: this.refresh
+      refresh: this.refresh,
+      btnText: 'Add user',
+      formItems: [
+        {
+          name: 'user_name',
+          key: 'user_name',
+          rules: [
+            {required: true, message: 'Please input your username!'},
+          ],
+          render: function () {
+            return (<Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Username"/>)
+          }
+        },
+        {
+          name: 'user_pwd',
+          key: 'user_pwd',
+          rules: [
+            {required: true, message: 'Please input your password!'},
+            {
+              validator: function checkConfirmPassword(rule, value, callback) {
+                // console.log(this)
+                const form = this.props.form;
+                const pwd = form.getFieldValue('user_pwd_twice');
+                if (value && pwd && value !== pwd) {
+                  callback('Two passwords that you enter is inconsistent!');
+                } else {
+                  callback();
+                }
+              }
+            }
+          ],
+          render: function () {
+            return (
+              <Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>} placeholder="new password"/>)
+          }
+        },
+        {
+          name: 'user_pwd',
+          key: 'user_pwd_twice',
+          rules: [
+            {required: true, message: 'Please input your password!'},
+            {
+              validator: function checkConfirmPassword(rule, value, callback) {
+                const form = this.props.form;
+                const pwd = form.getFieldValue('user_pwd');
+                if (value && pwd && value !== pwd) {
+                  callback('Two passwords that you enter is inconsistent!');
+                } else {
+                  callback();
+                }
+              }
+            }
+          ],
+          render: function () {
+            return (<Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                           placeholder="confirm password"/>)
+          }
+        },
+        {
+          "name": "type",
+          "key": "type",
+          rules: [{required: true, message: 'Please select your gender!'}],
+          render: function () {
+            return (
+              <Select
+                showSearch
+                style={{width: 200}}
+                placeholder="Select user type"
+                optionFilterProp="children"
+                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+              >
+                <Option value="10">admin</Option>
+                <Option value="0">ordinary</Option>
+              </Select>
+            )
+          }
+        }
+      ],
+      submit: {
+        btnText: 'create',
+        handleSubmit: function (values) {
+          fetchAndNotification({
+            url: 'user/user',
+            method: 'post',
+            params: values,
+            notifications: {
+              title: 'create Action',
+              success: `创建${values.user_name} 操作成功！`,
+              error: `创建${values.user_name} 操作失败！`,
+            },
+          }).then((result) => {
+            //when the fetch successfully ,refresh the table
+            this.props.form.resetFields();
+            this.props.refresh()
+          })
+        }
+      }
+      ,
+      modal: {
+        title: 'Add User'
+      }
+      ,
+      checkConfirmPassword: () => {
+        console.log()
+      }
     }
   };
 
