371c7669d4a2f101aee8641f3ef80292ca2da839
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-09-12 17:56:32 +0800

    add host post

--- src/routes/host/index.js
+++ src/routes/host/index.js
@@ -5,12 +5,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "dva";
 import DataTable from "../../components/BasicTable/DataTable";
-import {Modal, Row, Col, Card, Button, Menu, Dropdown, Icon} from "antd";
-import {Link} from "dva/router";
+import {Modal, Row, Col, Card, Button, Input, Select, Icon, Switch} from "antd";
 import DropOption from "../../components/DropOption/DropOption";
 import BatchModal from "../../components/modals/BatchModal";
 import {fetchAndNotification} from "../../services/restfulService";
-import {ActionCollections} from "../../components/host/ActionCollections";
+import ModalForm from "../../components/modalForm/ModalForm";
 import {ClusterList} from "../../components/clusterList/ClusterList";
 
 const confirm = Modal.confirm;
@@ -19,6 +18,240 @@ class HostPage extends React.Component {
   // constructor (props) {
   //   super(props)
   // }
+
+  componentWillMount() {
+
+    const formItems = {
+      user_name: {
+        name: '主机',
+        key: 'servername',
+        rules: [
+          {
+            required: true, message: 'Please input servername!'
+          },
+        ],
+        render: function () {
+          return (<Input prefix={<Icon type="" style={{fontSize: 13}}/>} placeholder="servername"/>)
+        }
+      },
+      user_pwd: {
+        name: '用户名',
+        key: 'username',
+        // updateValueFlag: false,
+        rules: [
+          {required: true, message: 'Please input username!'},
+        ],
+        render: function () {
+          return (
+            <Input type="text" prefix={<Icon type="user" style={{fontSize: 13}}/>}
+                   placeholder="username"/>)
+        }
+      },
+      user_pwd_twice: {
+        name: '密码',
+        key: 'passwd',
+        updateValueFlag: false,
+        rules: [
+          {required: true, message: 'Please input your password!'},
+        ],
+        render: function () {
+          return (<Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                         placeholder="password"/>)
+        }
+      },
+      group_id: {
+        "name": "主机组",
+        "key": "group_id",
+        rules: [{required: true, message: 'Please select your gender!'}],
+        render: () => {
+          return (
+            <Select
+              showSearch
+              style={{width: 200}}
+              placeholder="Select Host group"
+              optionFilterProp="children"
+              filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+              {
+                this.state.addHostModal.hostGroup.map(group => {
+                  return (
+                    <Option value={group.id}>{group.name}</Option>
+                  )
+                })
+              }
+            </Select>
+          )
+        }
+      },
+      rack_name: {
+        "name": "机架",
+        "key": "rack_name",
+        rules: [{required: true, message: 'Please select your gender!'}],
+        render: () => {
+          return (
+            <Select
+              showSearch
+              style={{width: 200}}
+              placeholder="Select Host group"
+              optionFilterProp="children"
+              filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+              {
+                this.state.addHostModal.hostRacks.map(rack => {
+                  return (
+                    <Option value={rack}>{rack}</Option>
+                  )
+                })
+              }
+            </Select>
+          )
+        }
+      },
+      publicip: {
+        "name": "外网IP",
+        "key": "publicip",
+        rules: [
+          {required: true, message: 'Please select your gender!'},
+          {
+            validator: function (rule, value, callback) {
+
+              const reg = new RegExp(/^([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})$/)
+              const check = reg.exec(value);
+              if (check) {
+                callback();
+              } else {
+                callback('请输入正确的IP!');
+              }
+            }
+          }
+        ],
+        render: () => {
+          return (
+            <Input type="text" prefix={<Icon type="user" style={{fontSize: 13}}/>}
+                   placeholder="外网IP"/>)
+
+        }
+      },
+      clusterip: {
+        "name": "集群IP",
+        "key": "clusterip",
+        rules: [
+          {required: true, message: 'Please select your gender!'},
+          {
+            validator: function (rule, value, callback) {
+
+              const reg = new RegExp(/^([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})$/)
+              const check = reg.exec(value);
+              if (check) {
+                callback();
+              } else {
+                callback('请输入正确的IP!');
+              }
+            }
+          }
+        ],
+        render: () => {
+          return (
+            <Input type="text" prefix={<Icon type="user" style={{fontSize: 13}}/>}
+                   placeholder="外网IP"/>)
+
+        }
+      },
+      backup_node: {
+        "name": "设为备份节点",
+        "key": "backup_node",
+        rules: [
+          {required: true, message: 'Please select your gender!'},
+        ],
+        render: () => {
+          return (
+            <Switch defaultChecked={true} onChange={e => {
+              console.log(e)
+            }}/>)
+        }
+      }
+    };
+
+    this.setState({
+      addHostModal: {
+        id: '',
+        cluster: this.props.modalProps.defaultCluster,
+        refresh: this.refresh,
+        btnText: 'Add Host',
+        btnTextShow: true,
+        formItems: formItems,
+        submit: {
+          btnText: 'create',
+          // should use function instead of es6 =>{} ,make sure get modalForm's current this
+          handleSubmit: async function (values) {
+
+            console.log(values)
+            await fetchAndNotification({
+              url: `ceph/clusters/${this.props.cluster.id}/servers/`,
+              method: 'post',
+              api: 'v2',
+              params: {
+                ...values,
+                backup_node: true
+              },
+              notifications: {
+                title: 'create Action',
+                success: `创建${values.servername} 操作成功！`,
+                error: `创建${values.servername} 操作失败！`,
+              },
+            });
+
+            this.props.form.resetFields();
+            this.props.refresh();
+          }
+        },
+        modalTitle: 'Add User',
+        modalVisible: false,
+        spinning: false,
+        hostGroup: [],
+        hostRacks: [],
+        handleModalShow: async () => {
+          this.setState(prevState => {
+            const addHostModal = prevState.addHostModal;
+            addHostModal.modalVisible = true;
+            addHostModal.cluster = this.props.modalProps.defaultCluster;
+            return {
+              addHostModal
+            }
+          });
+
+          const result = await Promise.all([
+            fetchAndNotification({
+              url: `ceph/clusters/${this.props.modalProps.defaultCluster.id}/groups/`,
+              api: 'v2'
+            }),
+            fetchAndNotification({
+              url: `ceph/clusters/${this.props.modalProps.defaultCluster.id}/racks`,
+              api: 'v2'
+            })
+          ]);
+
+          this.setState(prevState => {
+            const addHostModal = prevState.addHostModal;
+            addHostModal.hostGroup = result[0].data.items;
+            addHostModal.hostRacks = result[1].data.items;
+            return {
+              addHostModal: prevState.addHostModal
+            }
+          });
+        },
+        handleModalHide: () => {
+          this.setState(prevState => {
+            const addHostModal = prevState.addHostModal;
+            addHostModal.modalVisible = false;
+            return {
+              addHostModal
+            }
+          })
+        }
+      }
+    })
+  }
 
   componentDidMount() {
   }
@@ -298,9 +531,9 @@ class HostPage extends React.Component {
   };
 
   //define common props of actions btn
-  actionCollectionsProps = {
-    refresh: this.refresh
-  };
+  // actionCollectionsProps = {
+  //   refresh: this.refresh
+  // };
 
   render() {
     this.init();
@@ -321,7 +554,9 @@ class HostPage extends React.Component {
                   Batch Action
                 </Button>
                 {/*list a sort of actions*/}
-                <ActionCollections {...this.actionCollectionsProps}/>
+                {/*<ActionCollections {...this.actionCollectionsProps}/>*/}
+
+                <ModalForm {...this.state.addHostModal}/>
               </div>
               <DataTable {...this.tableDataProps} />
             </Card>
--- src/routes/userMgmt/index.js
+++ src/routes/userMgmt/index.js
@@ -23,91 +23,95 @@ class UserMgmt extends React.Component {
 
   componentWillMount() {
 
+    const formItems = {
+      user_name: {
+        name: 'user_name',
+        key: 'user_name',
+        rules: [
+          {
+            required: true, message: 'Please input your username!'
+          },
+        ],
+        render: function () {
+          return (<Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Username"/>)
+        }
+      },
+      user_pwd: {
+        name: 'user_pwd',
+        key: 'user_pwd',
+        updateValueFlag: false,
+        rules: [
+          {required: true, message: 'Please input your password!'},
+          {
+            validator: function checkConfirmPassword(rule, value, callback) {
+              // console.log(this)
+              const form = this.props.form;
+              const pwd = form.getFieldValue('user_pwd_twice');
+              if (value && pwd && value !== pwd) {
+                callback('Two passwords that you enter is inconsistent!');
+              } else {
+                callback();
+              }
+            }
+          }
+        ],
+        render: function () {
+          return (
+            <Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                   placeholder="new password"/>)
+        }
+      },
+      user_pwd_twice: {
+        name: 'user_pwd_twice',
+        key: 'user_pwd_twice',
+        updateValueFlag: false,
+        rules: [
+          {required: true, message: 'Please input your password!'},
+          {
+            validator: function checkConfirmPassword(rule, value, callback) {
+              const form = this.props.form;
+              const pwd = form.getFieldValue('user_pwd');
+              if (value && pwd && value !== pwd) {
+                callback('Two passwords that you enter is inconsistent!');
+              } else {
+                callback();
+              }
+            }
+          }
+        ],
+        render: function () {
+          return (<Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                         placeholder="confirm password"/>)
+        }
+      },
+      type: {
+        "name": "type",
+        "key": "type",
+        rules: [{required: true, message: 'Please select your gender!'}],
+        render: function () {
+          return (
+            <Select
+              showSearch
+              style={{width: 200}}
+              placeholder="Select user type"
+              optionFilterProp="children"
+              filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+              <Option value="10">admin</Option>
+              <Option value="0">ordinary</Option>
+            </Select>
+          )
+        }
+      }
+    };
+
     this.setState({
       addUserModal: {
         id: '',
         refresh: this.refresh,
         btnText: 'Add user',
         btnTextShow: true,
-        formItems: {
-          user_name: {
-            name: 'user_name',
-            key: 'user_name',
-            // initialValue: 1,
-            rules: [
-              {
-                required: true, message: 'Please input your username!'
-              },
-            ],
-            render: function () {
-              return (<Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Username"/>)
-            }
-          },
-          user_pwd: {
-            name: 'user_pwd',
-            key: 'user_pwd',
-            rules: [
-              {required: true, message: 'Please input your password!'},
-              {
-                validator: function checkConfirmPassword(rule, value, callback) {
-                  // console.log(this)
-                  const form = this.props.form;
-                  const pwd = form.getFieldValue('user_pwd_twice');
-                  if (value && pwd && value !== pwd) {
-                    callback('Two passwords that you enter is inconsistent!');
-                  } else {
-                    callback();
-                  }
-                }
-              }
-            ],
-            render: function () {
-              return (
-                <Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>} placeholder="new password"/>)
-            }
-          },
-          user_pwd_twice: {
-            name: 'user_pwd_twice',
-            key: 'user_pwd_twice',
-            rules: [
-              {required: true, message: 'Please input your password!'},
-              {
-                validator: function checkConfirmPassword(rule, value, callback) {
-                  const form = this.props.form;
-                  const pwd = form.getFieldValue('user_pwd');
-                  if (value && pwd && value !== pwd) {
-                    callback('Two passwords that you enter is inconsistent!');
-                  } else {
-                    callback();
-                  }
-                }
-              }
-            ],
-            render: function () {
-              return (<Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
-                             placeholder="confirm password"/>)
-            }
-          },
-          type: {
-            "name": "type",
-            "key": "type",
-            rules: [{required: true, message: 'Please select your gender!'}],
-            render: function () {
-              return (
-                <Select
-                  showSearch
-                  style={{width: 200}}
-                  placeholder="Select user type"
-                  optionFilterProp="children"
-                  filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                >
-                  <Option value="10">admin</Option>
-                  <Option value="0">ordinary</Option>
-                </Select>
-              )
-            }
-          }
-        },
+        formItems: formItems,
         submit: {
           btnText: 'create',
           // should use function instead of es6 =>{} ,make sure get modalForm's current this
@@ -159,87 +163,7 @@ class UserMgmt extends React.Component {
         refresh: this.refresh,
         btnTextShow: false,
         type: 'edit',
-        formItems: {
-          user_name: {
-            name: 'user_name',
-            key: 'user_name',
-            rules: [
-              {
-                required: true, message: 'Please input your username!'
-              },
-            ],
-            render: function () {
-              return (<Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Username"/>)
-            }
-          },
-          user_pwd: {
-            name: 'user_pwd',
-            key: 'user_pwd',
-            updateValueFlag: false,
-            rules: [
-              {required: true, message: 'Please input your password!'},
-              {
-                validator: function checkConfirmPassword(rule, value, callback) {
-                  // console.log(this)
-                  const form = this.props.form;
-                  const pwd = form.getFieldValue('user_pwd_twice');
-                  if (value && pwd && value !== pwd) {
-                    callback('Two passwords that you enter is inconsistent!');
-                  } else {
-                    callback();
-                  }
-                }
-              }
-            ],
-            render: function () {
-              return (
-                <Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
-                       placeholder="new password"/>)
-            }
-          },
-          user_pwd_twice: {
-            name: 'user_pwd_twice',
-            key: 'user_pwd_twice',
-            updateValueFlag: false,
-            rules: [
-              {required: true, message: 'Please input your password!'},
-              {
-                validator: function checkConfirmPassword(rule, value, callback) {
-                  const form = this.props.form;
-                  const pwd = form.getFieldValue('user_pwd');
-                  if (value && pwd && value !== pwd) {
-                    callback('Two passwords that you enter is inconsistent!');
-                  } else {
-                    callback();
-                  }
-                }
-              }
-            ],
-            render: function () {
-              return (<Input type="password" prefix={<Icon type="lock" style={{fontSize: 13}}/>}
-                             placeholder="confirm password"/>)
-            }
-          },
-          type: {
-            "name": "type",
-            "key": "type",
-            rules: [{required: true, message: 'Please select your gender!'}],
-            render: function () {
-              return (
-                <Select
-                  showSearch
-                  style={{width: 200}}
-                  placeholder="Select user type"
-                  optionFilterProp="children"
-                  filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                >
-                  <Option value="10">admin</Option>
-                  <Option value="0">ordinary</Option>
-                </Select>
-              )
-            }
-          }
-        },
+        formItems: formItems,
         submit: {
           btnText: 'update',
           // should use function instead of es6 =>{} ,make sure get modalForm's current this
